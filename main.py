81b67bdcfb436d8ab65e5a946a0d51e0a6d6e53a
Sean Davran <seandavran@10-248-45-160.wireless.oregonstate.edu>
2024-11-06 18:28:23 -0800

    fix the issue with the ports

--- main.py
+++ main.py
@@ -420,4 +420,4 @@ def get_user_reviews(user_id):
 if __name__ == '__main__':
     init_db()
     create_tables(db)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
